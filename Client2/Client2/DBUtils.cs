f3b634871499998e5cbcae617e8a3f3e227b1df7
Gleb Popov <gorlanandroid@gmail.com>
2020-02-15 20:50:29 +0700

    Added two clients

---
+++ Client2/Client2/DBUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Tutorial.SqlConn
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "192.168.31.164";
+            int port = 3306;
+            string database = "adam";
+            string username = "root";
+            string password = "password";
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+
+    }
+}
---
+++ Client2/Client2/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace Client2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.get0 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.get7 = new System.Windows.Forms.Button();
+            this.get6 = new System.Windows.Forms.Button();
+            this.get5 = new System.Windows.Forms.Button();
+            this.get4 = new System.Windows.Forms.Button();
+            this.get3 = new System.Windows.Forms.Button();
+            this.get2 = new System.Windows.Forms.Button();
+            this.get1 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.disconnect = new System.Windows.Forms.Button();
+            this.connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // get0
+            // 
+            this.get0.Location = new System.Drawing.Point(255, 101);
+            this.get0.Name = "get0";
+            this.get0.Size = new System.Drawing.Size(102, 30);
+            this.get0.TabIndex = 25;
+            this.get0.Text = "Get";
+            this.get0.UseVisualStyleBackColor = true;
+            this.get0.Click += new System.EventHandler(this.Get0_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(86, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 30);
+            this.textBox1.TabIndex = 24;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Ch0:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "IP:";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Culture = new System.Globalization.CultureInfo("en-001");
+            this.maskedTextBox1.Location = new System.Drawing.Point(86, 18);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(148, 30);
+            this.maskedTextBox1.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 26;
+            // 
+            // get7
+            // 
+            this.get7.Location = new System.Drawing.Point(255, 446);
+            this.get7.Name = "get7";
+            this.get7.Size = new System.Drawing.Size(102, 30);
+            this.get7.TabIndex = 47;
+            this.get7.Text = "Get";
+            this.get7.UseVisualStyleBackColor = true;
+            this.get7.Click += new System.EventHandler(this.Get7_Click);
+            // 
+            // get6
+            // 
+            this.get6.Location = new System.Drawing.Point(255, 394);
+            this.get6.Name = "get6";
+            this.get6.Size = new System.Drawing.Size(102, 30);
+            this.get6.TabIndex = 46;
+            this.get6.Text = "Get";
+            this.get6.UseVisualStyleBackColor = true;
+            this.get6.Click += new System.EventHandler(this.Get6_Click);
+            // 
+            // get5
+            // 
+            this.get5.Location = new System.Drawing.Point(255, 337);
+            this.get5.Name = "get5";
+            this.get5.Size = new System.Drawing.Size(102, 30);
+            this.get5.TabIndex = 45;
+            this.get5.Text = "Get";
+            this.get5.UseVisualStyleBackColor = true;
+            this.get5.Click += new System.EventHandler(this.Get5_Click);
+            // 
+            // get4
+            // 
+            this.get4.Location = new System.Drawing.Point(255, 287);
+            this.get4.Name = "get4";
+            this.get4.Size = new System.Drawing.Size(102, 30);
+            this.get4.TabIndex = 44;
+            this.get4.Text = "Get";
+            this.get4.UseVisualStyleBackColor = true;
+            this.get4.Click += new System.EventHandler(this.Get4_Click);
+            // 
+            // get3
+            // 
+            this.get3.Location = new System.Drawing.Point(255, 243);
+            this.get3.Name = "get3";
+            this.get3.Size = new System.Drawing.Size(102, 30);
+            this.get3.TabIndex = 43;
+            this.get3.Text = "Get";
+            this.get3.UseVisualStyleBackColor = true;
+            this.get3.Click += new System.EventHandler(this.Get3_Click);
+            // 
+            // get2
+            // 
+            this.get2.Location = new System.Drawing.Point(255, 200);
+            this.get2.Name = "get2";
+            this.get2.Size = new System.Drawing.Size(102, 30);
+            this.get2.TabIndex = 42;
+            this.get2.Text = "Get";
+            this.get2.UseVisualStyleBackColor = true;
+            this.get2.Click += new System.EventHandler(this.Get2_Click);
+            // 
+            // get1
+            // 
+            this.get1.Location = new System.Drawing.Point(255, 153);
+            this.get1.Name = "get1";
+            this.get1.Size = new System.Drawing.Size(102, 30);
+            this.get1.TabIndex = 41;
+            this.get1.Text = "Get";
+            this.get1.UseVisualStyleBackColor = true;
+            this.get1.Click += new System.EventHandler(this.Get1_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(86, 446);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(148, 30);
+            this.textBox8.TabIndex = 40;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(86, 394);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(148, 30);
+            this.textBox7.TabIndex = 39;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(86, 337);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(148, 30);
+            this.textBox6.TabIndex = 38;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(86, 287);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(148, 30);
+            this.textBox5.TabIndex = 37;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(86, 243);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 30);
+            this.textBox4.TabIndex = 36;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(86, 200);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(148, 30);
+            this.textBox3.TabIndex = 35;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(86, 153);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 30);
+            this.textBox2.TabIndex = 34;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(15, 449);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 25);
+            this.label9.TabIndex = 33;
+            this.label9.Text = "Ch7:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 397);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 25);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "Ch6:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 342);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 25);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Ch5:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 25);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Ch4:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 25);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Ch3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 25);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Ch2:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 156);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 25);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Ch1:";
+            // 
+            // disconnect
+            // 
+            this.disconnect.Location = new System.Drawing.Point(255, 54);
+            this.disconnect.Name = "disconnect";
+            this.disconnect.Size = new System.Drawing.Size(102, 30);
+            this.disconnect.TabIndex = 49;
+            this.disconnect.Text = "Disconnect";
+            this.disconnect.UseVisualStyleBackColor = true;
+            this.disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(255, 18);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(102, 30);
+            this.connect.TabIndex = 48;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 498);
+            this.Controls.Add(this.disconnect);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.get7);
+            this.Controls.Add(this.get6);
+            this.Controls.Add(this.get5);
+            this.Controls.Add(this.get4);
+            this.Controls.Add(this.get3);
+            this.Controls.Add(this.get2);
+            this.Controls.Add(this.get1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.get0);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button get0;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button get7;
+        private System.Windows.Forms.Button get6;
+        private System.Windows.Forms.Button get5;
+        private System.Windows.Forms.Button get4;
+        private System.Windows.Forms.Button get3;
+        private System.Windows.Forms.Button get2;
+        private System.Windows.Forms.Button get1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button disconnect;
+        private System.Windows.Forms.Button connect;
+    }
+}
+
